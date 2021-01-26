1832fad34f288f50481941475f9eac7d10c13eb4
Doriane974 <doriane.bedier974@gmail.com>
2021-01-26 17:48:16 +0100

    questions 4 et 5 , mais pas finies

--- modules/open_disgraph.py
+++ modules/open_disgraph.py
@@ -11,14 +11,28 @@ class node:
         self.parents = parents
         self.children = children
 
-class open_disgraph: #for open directed graph
-        def __init__(self, inputs, outputs, nodes):
+    def __str__(self):
+        return "(" + str(self.id) +"," + self.label +","+ str(self.parents) + "," + str(self.children) + ")"
+        '''
+        output : str ; the string given by str(node)
+        '''
 
-        '''
-        inputs: int list; the ids of the input nodes
-        outputs: int list; the ids of the output nodes
-        nodes: node list;
-        '''
+class open_digraph: #for open directed graph
+    def __init__(self, inputs, outputs, nodes):
+        #inputs: int list; the ids of the input nodes
+        #outputs: int list; the ids of the output nodes
+        #nodes: node list;
         self.inputs = inputs
         self.outputs = outputs
         self.nodes = {node.id:node for node in nodes} # self.nodes: <int,node> dict
+    def __str__(self):
+        return "(" + str(self.inputs) +"," + str(self.outputs) +","+ str(print(self.nodes)) + ")"
+
+    def empty():
+        '''
+
+        '''
+        return open_digraph([],[],[])
+'''def __str__(self):
+    print( "id =" , self.inputs , "," , "liste des output : " , self.outputs , "nodes : " , self.nodes)
+'''
--- worksheet.py
+++ worksheet.py
@@ -1,1 +1,12 @@
 print('hello world');
+
+from modules.open_disgraph import *
+
+n0 = node(0, 'papa', [], [1]);
+n1 = node(1, 'maman', [], [1]);
+n2 = node(1, 'enfant',['papa', 'maman'],[])
+
+g = open_digraph([0],[1],[n0,n1]);
+
+print(g)
+print(open_digraph.empty()) #Question 5
