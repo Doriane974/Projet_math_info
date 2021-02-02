e50d54daba7c7eeaa835fe5aacb39cdfa882cf94
Doriane974 <doriane.bedier974@gmail.com>
2021-02-02 14:22:25 +0100

    coorection de l'indentation

--- tests/open_digraph_test.py
+++ tests/open_digraph_test.py
@@ -25,75 +25,74 @@ class InitTest(unittest.TestCase):
         self.assertEqual(self.g.outputs, [3])
         #self.assertEqual(g.nodes, [n0,n1])
 
-    class NodeTest(unittest.TestCase):
+class NodeTest(unittest.TestCase):
 
-        def setUp(self):                    #dans l'enonce
-            self.n0 = node(0, 'a', [], [1]) #dans l'enonce
-            self.n1 = node(1, 'b', [0], []) #ligne rajoutée
+    def setUp(self):                    #dans l'enonce
+        self.n0 = node(0, 'a', [], [1]) #dans l'enonce
+        self.n1 = node(1, 'b', [0], []) #ligne rajoutée
 
-        def test_get_id(self):              #dans l'enonce
-            self.assertEqual(self.n0.get_id(), 0)   #dans l'enonce
-            self.assertEqual(self.n1.get_id(), 1)
+    def test_get_id(self):              #dans l'enonce
+        self.assertEqual(self.n0.get_id(), 0)   #dans l'enonce
+        self.assertEqual(self.n1.get_id(), 1)
 
-        def test_get_label(self):           #dans l'enonce
-            self.assertEqual(self.n0.get_label(), 'a')  #dans l'enonce
-            self.assertEqual(self.n1.get_label(), 'b')
+    def test_get_label(self):           #dans l'enonce
+        self.assertEqual(self.n0.get_label(), 'a')  #dans l'enonce
+        self.assertEqual(self.n1.get_label(), 'b')
 
-        def test_copy(self):
-            self.n0 = node(0, 'a', [], [1])
-            self.n1 = node(1, 'b', [0], [])
-            n2 = self.n0.copy()
-            n2.label = 'c'
-            self.assertEqual(n2.id, self.n0.id)
-            self.assertNotEqual(n2.label, self.n0.label)
-            self.assertIsNot(n2, self.n0)
+    def test_copy(self):
+        self.n0 = node(0, 'a', [], [1])
+        self.n1 = node(1, 'b', [0], [])
+        n2 = self.n0.copy()
+        n2.label = 'c'
+        self.assertEqual(n2.id, self.n0.id)
+        self.assertNotEqual(n2.label, self.n0.label)
+        self.assertIsNot(n2, self.n0)
 
-        def test_get_id(self):
-            self.n0 = node(0, 'a', [], [1])
-            self.assertEqual(self.n0.get_id(),0)
-            self.assertNotEqual(self.n0.get_id(),1)
+    def test_get_id(self):
+        self.n0 = node(0, 'a', [], [1])
+        self.assertEqual(self.n0.get_id(),0)
+        self.assertNotEqual(self.n0.get_id(),1)
 
-        def test_get_label(self):
-            self.n0 = node(0, 'a', [], [1])
-            self.assertEqual(self.n0.get_label(),'a')
-            self.assertNotEqual(self.n0.get_label(),'b')
+    def test_get_label(self):
+        self.n0 = node(0, 'a', [], [1])
+        self.assertEqual(self.n0.get_label(),'a')
+        self.assertNotEqual(self.n0.get_label(),'b')
 
-        def test_get_parents_ids(self):
-            self.n0 = node(0, 'a', [2], [1])
-            self.assertEqual(self.n0.get_parent_ids(),[2])
-            self.n2 = node(0, 'a', [0], [1])
-            self.assertNotEqual(self.n1.get_parent_ids(),[2])
+    def test_get_parents_ids(self):
+        self.n0 = node(0, 'a', [2], [1])
+        self.assertEqual(self.n0.get_parent_ids(),[2])
+        self.n2 = node(0, 'a', [0], [1])
+        self.assertNotEqual(self.n1.get_parent_ids(),[2])
 
-        def test_get_children_ids(self):
-            self.n0 = node(0, 'a', [2], [1])
-            self.assertEqual(self.n0.get_parent_ids(),[1])
-            self.n2 = node(0, 'a', [0], [1])
-            self.assertNotEqual(self.n1.get_parent_ids(),[2])
+    def test_get_children_ids(self):
+        self.n0 = node(0, 'a', [2], [1])
+        self.assertEqual(self.n0.get_parent_ids(),[1])
+        self.n2 = node(0, 'a', [0], [1])
+        self.assertNotEqual(self.n1.get_parent_ids(),[2])
 
-    class DigraphTest(unittest.TestCase):
+class DigraphTest(unittest.TestCase):
 
-    	def setUp(self):
-            self.n0 = node(0, 'a', [], [1])
-            self.n1 = node(1, 'b', [0], [])
-            self.d0 = open_digraph([0],[1],[self.n0, self.n1])                     # to be completed
+    def setUp(self):
+        self.n0 = node(0, 'a', [], [1])
+        self.n1 = node(1, 'b', [0], [])
+        self.d0 = open_digraph([0],[1],[self.n0, self.n1])                     # to be completed
 
+    def test_copy(self):
+        self.n0 = node(0, 'a', [], [1])
+        self.n1 = node(1, 'b', [0], [])
+        self.d0 = open_digraph([0],[1],[self.n0, self.n1])
+        n2 = self.d0.copy()
+        d2.inputs = [1]
+        self.assertEqual(d2.outputs, self.d0.outputs)
+        self.assertNotEqual(d2.inputs, self.d0.inputs)
+        self.assertIsNot(d2,self.d0)
 
-    	def test_copy(self):
-            self.n0 = node(0, 'a', [], [1])
-            self.n1 = node(1, 'b', [0], [])
-            self.d0 = open_digraph([0],[1],[self.n0, self.n1])
-            n2 = self.d0.copy()
-            d2.inputs = [1]
-            self.assertEqual(d2.outputs, self.d0.outputs)
-            self.assertNotEqual(d2.inputs, self.d0.inputs)
-            self.assertIsNot(d2,self.d0)
-
-        def test_get_inputs_ids(self):
-            self.n0 = node(0, 'a', [], [1])
-            self.n1 = node(1, 'b', [0], [])
-            self.d0 = open_digraph([0],[1],[self.n0, self.n1])
-            self.assertEqual(self.d0.get_inputs_ids(),[0])
-            self.assertNotEqual(self.d0.get_inputs_ids(),[1])
+    def test_get_inputs_ids(self):
+        self.n0 = node(0, 'a', [], [1])
+        self.n1 = node(1, 'b', [0], [])
+        self.d0 = open_digraph([0],[1],[self.n0, self.n1])
+        self.assertEqual(self.d0.get_inputs_ids(),[0])
+        self.assertNotEqual(self.d0.get_inputs_ids(),[1])
 
 
 
