f56a3f7bd293dc2b58327f88883d4858679c784b
Doriane974 <doriane.bedier974@gmail.com>
2021-02-26 12:58:35 +0100

    exo4 TD 4

--- modules/Interface.py
+++ modules/Interface.py
@@ -34,15 +34,20 @@ def drawarrows(self, p1, p2):
     self.line([p1.n(), p2.n()], 'black')
 
 def drawnode(self, noeud, p, verbose = False):
-    self.ellipse((p.x-5, p.y-5, p.x+5, p.y+5), fill='white', outline='black')
-    self.text((p.x + 6 ,p.y - 6), noeud.get_label() , fill='black')
+    self.ellipse((p.x-10, p.y-10, p.x+10, p.y+10), fill='white', outline='black')
+    self.text((p.x - 2  ,p.y - 9), str(noeud.get_label()) , fill='red')
     if(verbose):
-        self.text((p.x - 6 ,p.y + 6), str(noeud.get_id()) , fill='black')
+        self.text((p.x-2 ,p.y ), str(noeud.get_id()) , fill='blue')
+
+def drawgraph(self, g, method='manual', node_pos=None, input_pos=None, output_pos=None):
+    
 
 
 
 
 ImageDraw.ImageDraw.arrows = drawarrows # we define the method 'arrows'
-# from the function 'arrows' above
+                                        # from the function 'arrows' above
 
 ImageDraw.ImageDraw.node = drawnode
+
+#ImageDraw.ImageDraw.graph = drawgraph
