8398a96f45377cfad20398d3bd58c2e457373793
Doriane97 <doriane.bedier974@gmail.com>
2021-03-02 11:11:44 +0100

    doc utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,14 +1,19 @@
 import operator
 import random
 
+'''Fonction qui retire toutes les occurences de x dans l
+arguments : l : 'a list
+            x : 'a
+return : none '''
 def remove_all(l,x):                                      # retire toutes les occurrences de x dans l
-    #for i in l:
-    #    if i == x :
-    #        l.remove(i)
     while x in l:
         l.remove(x)
     return l
 
+'''Fonction qui compte toutes les occurences de x dans l
+arguments : l : 'a list
+            x : 'a
+return : int '''
 def count_occurrences(l,x):                               # compte toutes les occurrences de x dans l
     cpt = 0
     for i in l :
@@ -16,13 +21,20 @@ def count_occurrences(l,x):                               # compte toutes les oc
             cpt += 1
     return cpt
 
-
+'''Fonction qui renvoie une liste d'entiers aléatoire
+argument : bound : int : la borne maximum de l'intervalle dans lequel on choisit les élements de la liste, bound exclus
+return : int list '''
 def random_int_list(n, bound):                             # renvoie une liste d'entiers aléatoire
     l=[0]*n
     for i in range(0, n):
         l[i] = random.randrange(0,bound)
     return l
 
+'''Fonction qui renvoie une matrice carrée d'entier aléatoire
+argument : n : int : taille de la matrice
+           bound : int : la borne maximum de l'intervalle dans lequel on choisit les élements de la liste, bound exclus
+           null_diag : bool, par défaut True, si True : Diagonale de la matrice nulle
+return : int list list '''
 def random_int_matrix(n,bound, null_diag=True):            # renvoie une matrice d'entier aléatoire
     l=[0]*n
     for i in range(0,n):
@@ -32,6 +44,11 @@ def random_int_matrix(n,bound, null_diag=True):            # renvoie une matrice
             l[i][i]=0
     return l
 
+'''Fonction qui renvoie une matrice carrée d'entier aléatoire symétrique
+argument : n : int : taille de la matrice
+           bound : int : la borne maximum de l'intervalle dans lequel on choisit les élements de la liste, bound exclus
+           null_diag : bool, par défaut True, si True : Diagonale de la matrice nulle
+return : int list list '''
 def random_symetric_int_matrix(n, bound, null_diag=True):   #renvoie une matrice symétrique d'entier aléatoire
     l = random_int_matrix(n, bound, null_diag)
     for i in range(0,n):
@@ -39,6 +56,11 @@ def random_symetric_int_matrix(n, bound, null_diag=True):   #renvoie une matrice
             l[j][i]=l[i][j]
     return l
 
+'''Fonction qui renvoie une matrice carrée d'entier aléatoire correspondant a un graphe orienté
+argument : n : int : taille de la matrice
+           bound : int : la borne maximum de l'intervalle dans lequel on choisit les élements de la liste, bound exclus
+           null_diag : bool, par défaut True, si True : Diagonale de la matrice nulle
+return : int list list '''
 def random_oriented_int_matrix(n, bound, null_diag=True):   #renvoie une matrice d'entier aléatoire correspondant a un graphe orienté
     l=random_int_matrix(n, bound, null_diag)
     for i in range(0,n):
@@ -51,6 +73,11 @@ def random_oriented_int_matrix(n, bound, null_diag=True):   #renvoie une matrice
                 l[j][i] = 0
     return l
 
+'''Fonction qui renvoie une matrice triangulaire carrée d'entier aléatoire 
+argument : n : int : taille de la matrice
+           bound : int : la borne maximum de l'intervalle dans lequel on choisit les élements de la liste, bound exclus
+           null_diag : bool, par défaut True, si True : Diagonale de la matrice nulle
+return : int list list '''
 def random_triangular_int_matrix(n, bound, null_diag=True): #renvoie une matrice triangulaire d'entier aléatoire
     l=random_int_matrix(n, bound, null_diag)
     for i in range(0,n):
